37ba04302b4036ba4b70f1405973201ddaf9c46c
Mark Zsombor <markszsombor@gmail.com>
2018-10-08 12:13:17 -0700

    second iteration of second problem Bob

--- bob/bob.exs
+++ bob/bob.exs
@@ -1,16 +1,28 @@
 defmodule Bob do
+  @numbers ~r(^[^a-zA-ZА-Яа-я]*$)
+
   def hey(input) do
     cond do     
       String.trim(input) == "" -> "Fine. Be that way!"
-      # check the number only possibilities
-      input =~ ~r(^[^a-zA-ZА-Яа-я]*$) && String.ends_with? input, "?" -> "Sure."
-      input =~ ~r(^[^a-zA-ZА-Яа-я]*$) -> "Whatever."
-      String.upcase(input) == input && String.ends_with? input, "?" -> "Calm down, I know what I'm doing!"
-      String.upcase(input) == input -> "Whoa, chill out!"
-      String.ends_with? input, "?" -> "Sure."
+      # check the number only possibilities first
+      only_numbers?(input) && is_question?(input) -> "Sure."
+      only_numbers?(input) -> "Whatever."
+      is_uppercase?(input) && is_question?(input) -> "Calm down, I know what I'm doing!"
+      is_uppercase?(input) -> "Whoa, chill out!"
+      is_question?(input) -> "Sure."
       true -> "Whatever."
     end
   end
-end
 
-Bob.hey("44")
+  def only_numbers?(input) do
+    input =~ @numbers
+  end
+
+  def is_uppercase?(input) do
+    String.upcase(input) == input
+  end
+
+  def is_question?(input) do
+    String.ends_with? input, "?"
+  end
+end
